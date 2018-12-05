70010b6b52645ea09617458497e4c28a1707a9c6
pipicanim <denizhanerdem@gmail.com>
2018-12-05 22:33:53 +0300

    day4 guard sleep times ready to process

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,43 @@
+extern crate regex;
+#[macro_use]
+extern crate itertools;
+use itertools::Itertools;
+use regex::Regex;
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
 fn main() {
-    println!("Hello, world!");
+    let br = BufReader::new(File::open("input.txt").unwrap());
+    let times: Vec<String> = br.lines().map(|x| x.unwrap()).sorted();
+    let re = Regex::new(
+        r"\[\d{4}-(\d{2}-\d{2}) \d{2}:(\d{2})\] (falls asleep|wakes up|Guard #(\d+) begins shift)",
+    )
+    .unwrap();
+    let mut active_guard: usize = 0;
+    let mut sleep_time: usize = 0;
+    for t in times {
+        for cap in re.captures_iter(&t) {
+            match cap.get(4) {
+                Some(gid) => {
+                    active_guard = gid.as_str().parse::<usize>().unwrap();
+                    println!("new guard {}", active_guard);
+                }
+                None => {
+                    if cap[3].contains("falls asleep") {
+                        sleep_time = cap[2].parse::<usize>().unwrap();
+                    } else {
+                        let wake_time = cap[2].parse::<usize>().unwrap();
+                        println!(
+                            "Guard {} slept from {} to {}",
+                            active_guard, sleep_time, wake_time
+                        );
+                        //TODO work with guard sleep times
+                    }
+                }
+            }
+        }
+    }
 }
