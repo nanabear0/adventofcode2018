80f35bba764a4c01ddd3296642960ff09199a1c5
pipicanim <denizhanerdem@gmail.com>
2018-12-08 01:08:13 +0300

    reimplemented day 1 parts 1 and 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,2 +1,30 @@
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+fn day11() {
+    let br = BufReader::new(File::open("input.txt").unwrap());
+    println!(
+        "{}",
+        br.lines()
+            .map(|x| x.unwrap().parse::<i32>().unwrap() as i32)
+            .sum::<i32>()
+    );
+}
+fn day12() {
+    let br = BufReader::new(File::open("input.txt").unwrap());
+    let v: Vec<i32> = br.lines().map(|x| x.unwrap().parse().unwrap()).collect();
+    let mut set: HashSet<i32> = HashSet::new();
+    let mut result = 0;
+    for (i, x) in v.iter().cycle().enumerate() {
+        result += x;
+        if !set.insert(result) {
+            println!("{} {}", result, i);
+            break;
+        }
+    }
+}
 fn main() {
+    day12();
 }
