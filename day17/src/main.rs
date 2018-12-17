2111dea32140b35db6b07d62571e844f8421d354
pipicanim <denizhanerdem@gmail.com>
2018-12-17 20:49:47 +0300

    visualizations

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,11 +1,13 @@
+extern crate image;
 extern crate regex;
+use image::{ImageBuffer, Rgb};
 use regex::Regex;
-use std::collections::{BTreeMap, HashMap, HashSet};
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::ops::Range;
 use std::time::{Duration, Instant};
+
+const CREATE_VISUALIZATIONS: bool = false;
 
 fn pos(y: usize, x: usize) -> usize {
     y * 2000 + x
@@ -22,40 +24,37 @@ fn unstabilize(y: usize, x: usize, well: &mut Vec<char>, max_y: usize) {
 }
 
 fn spread(y: usize, x: usize, well: &mut Vec<char>, max_y: usize) -> bool {
-    let mut stabilize;
-    if well[pos(y, x - 1)] == '.' {
-        stabilize = drip(y, x - 1, well, max_y);
+    let mut stabilize = if well[pos(y, x - 1)] == '.' {
+        drip(y, x - 1, well, max_y)
     } else {
-        stabilize = true;
-    }
+        true
+    };
+
     if well[pos(y, x + 1)] == '.' {
         stabilize = drip(y, x + 1, well, max_y) && stabilize;
-    } else {
-        stabilize = stabilize && true;
     }
+
     if stabilize {
         well[pos(y, x)] = '~';
     } else {
         unstabilize(y, x, well, max_y);
     }
-    return stabilize;
+    stabilize
 }
 
 fn drip(y: usize, x: usize, well: &mut Vec<char>, max_y: usize) -> bool {
     well[pos(y, x)] = '|';
     if y < max_y && well[pos(y + 1, x)] == '.' {
         if drip(y + 1, x, well, max_y) {
-            return spread(y, x, well, max_y);
+            spread(y, x, well, max_y)
         } else {
             unstabilize(y, x, well, max_y);
             false
         }
-    } else if well[pos(y + 1, x)] == '#' {
-        return spread(y, x, well, max_y);
-    } else if well[pos(y + 1, x)] == '~' {
-        return spread(y, x, well, max_y);
+    } else if well[pos(y + 1, x)] == '#' || well[pos(y + 1, x)] == '~' {
+        spread(y, x, well, max_y)
     } else {
-        return false;
+        false
     }
 }
 
@@ -65,10 +64,12 @@ fn main() {
     let regex = Regex::new(r"(.)=(\d+), (.)=(\d+)..(\d+)").unwrap();
     let mut well: Vec<char> = vec!['.'; 2000 * 2000];
     well[500] = '+';
+
     let mut min_y = usize::max_value();
     let mut max_y = usize::min_value();
     let mut min_x = usize::max_value();
     let mut max_x = usize::min_value();
+
     for input in br.lines().map(|x| x.unwrap()) {
         let caps = regex.captures(&input).unwrap();
         let id = caps[1].to_string();
@@ -94,8 +95,14 @@ fn main() {
             }
         }
     }
+
     drip(1, 500, &mut well, max_y);
-    // print_well(&well, &min_y, &max_y, &min_x, &max_x);
+
+    if CREATE_VISUALIZATIONS {
+        print_well(&well, min_y, max_y, min_x, max_x);
+        visualize(&well, min_y, max_y, min_x, max_x);
+    }
+
     println!(
         "part 1: {}",
         well.iter()
@@ -103,24 +110,51 @@ fn main() {
             .filter(|x| **x == '~' || **x == '|')
             .count()
     );
+
     println!(
-        "part 1: {}",
+        "part 2: {}",
         well.iter()
             .skip(min_y * 2000)
             .filter(|x| **x == '~')
             .count()
     );
+
     let d: Duration = now.elapsed();
     println!("> {}.{:03} seconds", d.as_secs(), d.subsec_millis());
 }
 
-#[allow(dead_code)]
-fn print_well(well: &Vec<char>, min_y: &usize, max_y: &usize, min_x: &usize, max_x: &usize) {
+fn print_well(well: &[char], min_y: usize, max_y: usize, min_x: usize, max_x: usize) {
     let mut file = File::create("foo.txt").unwrap();
-    for c in 0..=*max_y {
-        for r in *min_x..=(max_x + 1) {
+    for c in min_y..=max_y {
+        for r in (min_x - 1)..=(max_x + 1) {
             write!(file, "{}", well[c * 2000 + r]).unwrap();
         }
         writeln!(file).unwrap();
     }
+}
+
+fn visualize(well: &[char], min_y: usize, max_y: usize, min_x: usize, max_x: usize) {
+    let width = max_x - min_x + 3;
+    let height = max_y - min_y + 1;
+    let mut img: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::new(width as u32, height as u32);
+    for (y, c) in (min_y..=max_y).enumerate() {
+        for (x, r) in ((min_x - 1)..=(max_x + 1)).enumerate() {
+            match well[c * 2000 + r] {
+                '#' => {
+                    img.get_pixel_mut(x as u32, y as u32).data = [0, 0, 0];
+                }
+                '.' => {
+                    img.get_pixel_mut(x as u32, y as u32).data = [255, 255, 255];
+                }
+                '|' => {
+                    img.get_pixel_mut(x as u32, y as u32).data = [0, 255, 255];
+                }
+                '~' => {
+                    img.get_pixel_mut(x as u32, y as u32).data = [0, 0, 255];
+                }
+                _ => {}
+            }
+        }
+    }
+    img.save("bar.png").unwrap();
 }
